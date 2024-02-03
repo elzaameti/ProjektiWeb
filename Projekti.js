7d07203e0945d987c9c63b1d967d3de5d0f05bb6
ErzaKo <149580576+ErzaKo@users.noreply.github.com>
2024-02-03 23:47:48 +0100

    Update1

--- Projekti.js
+++ Projekti.js
@@ -1,51 +1,13 @@
-const users = [];
+var swiper = new Swiper(".swiper-conatiner", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
-document.getElementById('login-btn').addEventListener('click', toggleLoginForm);
-document.getElementById('signup-btn').addEventListener('click', toggleSignupForm);
-
-function toggleLoginForm() {
-    console.log('toggleLoginForm executed');
-    const loginFormContainer = document.getElementById('login-form-container');
-    const signupFormContainer = document.getElementById('signup-form-container');
-
-    loginFormContainer.style.display = 'block';
-    signupFormContainer.style.display = 'none';
-}
-
-function toggleSignupForm() {
-    console.log('toggleSignupForm executed');
-    const loginFormContainer = document.getElementById('login-form-container');
-    const signupFormContainer = document.getElementById('signup-form-container');
-
-    loginFormContainer.style.display = 'none';
-    signupFormContainer.style.display = 'block';
-}
-function login() {
-    const username = document.getElementById('login-username').value;
-    const password = document.getElementById('login-password').value;
-
-    // Check if the user exists
-    const user = users.find(u => u.username === username && u.password === password);
-
-    if (user) {
-        alert('Login successful');
-    } else {
-        alert('Invalid username or password');
-    }
-}
-
-function signup() {
-    const username = document.getElementById('signup-username').value;
-    const password = document.getElementById('signup-password').value;
-
-    // Check if the username is already taken
-    const existingUser = users.find(u => u.username === username);
-
-    if (existingUser) {
-        alert('Username already taken. Please choose another.');
-    } else {
-        // Add the new user to the database
-        users.push({ username, password });
-        alert('Signup successful. You can now login.');
-    }
-}
+    
